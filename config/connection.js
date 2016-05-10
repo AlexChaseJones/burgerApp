6ab738d60c535ebff96c0d50dbdce7147b0277f1
Alexander <alexchasejones@icloud.com>
2016-05-10 18:18:40 -0400

    Heroku connection changes

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,33 @@
 //============================================================
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-	host	: 'localhost',
-	user	: 'root',
-	password: '',
-	database: 'burgers_db'
-});
+// var connection = mysql.createConnection({
+// 	host	: 'localhost',
+// 	user	: 'root',
+// 	password: '',
+// 	database: 'burgers_db'
+// });
 
+// connection.connect(function(err) {
+// 	if (err) throw err;
+// 	console.log('connected as id ' + connection.threadId);
+// });
+
+//Sequelize Database setup
+//============================================================
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host	: 'localhost',
+		user	: 'root',
+		password: '',
+		database: 'burgers_db'
+	})	
+}
+
+//Create the model and define the schema using Sequelize
 connection.connect(function(err) {
 	if (err) throw err;
 	console.log('connected as id ' + connection.threadId);
