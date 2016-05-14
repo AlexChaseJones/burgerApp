17e971aacbd93e9fd326f99f9746370fd5715f84
Alexander <alexchasejones@icloud.com>
2016-05-14 11:50:16 -0400

    s

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars');
 
 app.use('/', routes);
-var PORT = process.env.port || 9000;
+var PORT = process.env.PORT || 9000;
 
 app.listen(PORT, function(){
 	console.log('App listening on PORT ' + PORT);
